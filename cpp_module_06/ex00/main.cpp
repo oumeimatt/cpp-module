b1dede1dc1ea2976adf0280d8c95b0437e0165fd
oumeimatt <oumelyoussefi@gmail.com>
2021-10-11 13:21:29 +0100

    ex00 module06

--- cpp_module_06/ex00/main.cpp
+++ cpp_module_06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oel-yous <oel-yous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 11:23:42 by oel-yous          #+#    #+#             */
-/*   Updated: 2021/10/10 17:49:31 by oel-yous         ###   ########.fr       */
+/*   Updated: 2021/10/11 13:20:21 by oel-yous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 #include <iomanip>
 #include <string>
 
-void    convertToInt(char **argv)
+char	getArgType(char **argv){
+	std::string s(argv[1]);
+	if (s.size() == 1 && !std::isdigit(s[0]))
+		return(s[0]);
+	else
+		return(0);
+}
+
+void    convertToInt(char **argv, char c)
 {
 	std::cout << "int: ";
+	if (getArgType(argv)){
+		std::cout << static_cast<int>(c) << std::endl;
+		return ;
+	}
 	try{
 	   int i = std::stoi(argv[1]);
 	   std::cout << static_cast<int> (i) << std::endl;
@@ -27,9 +39,13 @@ void    convertToInt(char **argv)
 	}
 }
 
-void    convertToFloat(char **argv)
+void    convertToFloat(char **argv, char c)
 {
 	std::cout << "float: ";
+	if (getArgType(argv)){
+		std::cout << static_cast<float> (c) << std::endl;
+		return ;
+	}
 	try{
 	   float i = std::stof(argv[1], 0);
 	   std::cout << std::fixed << std::setprecision(1) << static_cast<float> (i) << "f" << std::endl;
@@ -40,9 +56,13 @@ void    convertToFloat(char **argv)
 	}
 }
 
-void    convertToDouble(char **argv)
+void    convertToDouble(char **argv, char c)
 {
 	std::cout << "double: ";
+	if (getArgType(argv)){
+		std::cout << static_cast<double>(c)<< std::endl;
+		return ;
+	}
 	try{
 	   double i = std::stod(argv[1],0);
 	   std::cout << static_cast<double> (i) << std::endl;
@@ -53,9 +73,13 @@ void    convertToDouble(char **argv)
 	}
 }
 
-void    convertToChar(char **argv)
+void    convertToChar(char **argv, char c)
 {
 	std::cout << "char: ";
+	if (getArgType(argv)){
+		std::cout << c << std::endl;
+		return ;
+	}
 	try{
 		int i = std::stoi(argv[1]);
 		if (i < 0 || i > 255)
@@ -72,11 +96,13 @@ void    convertToChar(char **argv)
 		std::cout << "impossible "<< std::endl;
 	}
 }
+
 int main (int argc, char **argv){
 	if (argc > 1){
-		convertToChar(argv);
-		convertToInt(argv);
-		convertToFloat(argv);
-		convertToDouble(argv);
+		char c = getArgType(argv);
+		convertToChar(argv, c);
+		convertToInt(argv, c);
+		convertToFloat(argv, c);
+		convertToDouble(argv, c);
 	}
 }
